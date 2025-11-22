5cbd93d265c6c5644e365f1588befc5d52bb9661
tmourtada2025 <194477179+tmourtada2025@users.noreply.github.com>
2025-11-22 05:19:23 -0500

    Fix: Change ModelCard aspect ratio to 3/4 (12/16) for classic portrait look

--- Cyberchicmodels.ai-main/src/components/ModelCard.tsx
+++ Cyberchicmodels.ai-main/src/components/ModelCard.tsx
@@ -56,7 +56,7 @@ export function ModelCard({ model, onModelClick }: ModelCardProps) {
 
   return (
     <div
-      className="relative w-full aspect-[9/16] group cursor-pointer"
+      className="relative w-full aspect-[3/4] group cursor-pointer"
       onClick={handleCardClick}
     >
       <div className="absolute top-0 left-0 z-10 flex flex-col gap-2 transform -translate-x-2 -translate-y-2">
